0b13c5768553e8956f5ab5a9e4185b81be9cb4b0
Kathryn Hoelscher <khoelscher0625@gmail.com>
2020-01-18 20:38:36 +0000

    editing stylesheets

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -59,8 +59,5 @@ private
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 
 end
